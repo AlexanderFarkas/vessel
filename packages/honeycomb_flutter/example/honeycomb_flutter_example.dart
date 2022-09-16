4ee791cf5602cb6e3df5ae2c0cc80ac19c0b8b3a
AlexanderFarkas <app@funjoin.com>
2022-09-16 15:13:45 +0300

    feat!: removed fic from dependencies

---
+++ packages/honeycomb_flutter/example/honeycomb_flutter_example.dart
@@ -0,0 +1,6 @@
+import 'package:honeycomb_flutter/honeycomb_flutter.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/honeycomb_flutter/lib/honeycomb_flutter.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library honeycomb_flutter;
+
+export 'src/honeycomb_flutter_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
